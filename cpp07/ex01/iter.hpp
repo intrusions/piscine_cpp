1c6e715b3c5f05519bdb799dc148e0a2762a7c53
intrusions <xeloronkali@gmail.com>
2023-03-09 11:25:25 -0800

    cpp07 ex01 done

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jucheval <jucheval@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 11:12:13 by jucheval          #+#    #+#             */
+/*   Updated: 2023/03/09 11:21:06 by jucheval         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <stdint.h>
+
+template<typename T>
+void	iter(T *array, uint16_t size, void (*f)(T&)) {
+	
+	for (uint16_t i = 0; i < size; i++) {
+		f(array[i]);
+	}
+}
