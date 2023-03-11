b287ab765ead012fda52b5783a9ff5b40980f4bc
intrusions <xeloronkali@gmail.com>
2023-03-11 03:59:05 +0100

    cpp07 ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: jucheval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 11:54:52 by jucheval          #+#    #+#             */
-/*   Updated: 2023/03/10 23:22:59 by jucheval         ###   ########.fr       */
+/*   Updated: 2023/03/11 02:45:15 by jucheval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@ class Array {
 	public:
 		Array() : _arr(new T[0]), _len(0) {}
 
-		Array(uint32_t n) : _arr(new T[n]), _len(n - 1) {
+		Array(uint32_t n) : _arr(new T[n]), _len(n) {
 			for (uint32_t i = 0; i < n; i++)
-		_arr[i] = 0;
+				_arr[i] = 0;
 		}
 		
-		Array(Array const &obj) : _len(obj._len), _arr(new T[_len]) {
+		Array(Array const &obj) : _arr(new T[obj._len]), _len(obj._len) {
 			for (uint32_t i = 0; i < _len; i++)
-		_arr[i] = obj._arr[i];
+				_arr[i] = obj._arr[i];
 		}
 		
 		~Array() { delete[] _arr; }
@@ -47,5 +47,19 @@ class Array {
 			}
 			return (*this);
 		}
+
+		T	&operator[](uint32_t index) {
+			if (index < 0 || index >= _len)
+				throw InvalidIndex();
+			return (_arr[index]);
+		}
+
+		class InvalidIndex : public std::exception {
+			public:
+				std::string	InvalidId() const throw() {
+					return ("Invalid index");
+				}
+		};
+		
 		uint32_t	size() { return (_len); }
 };
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,13 +6,29 @@
 /*   By: jucheval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 11:18:03 by jucheval          #+#    #+#             */
-/*   Updated: 2023/03/10 21:26:43 by jucheval         ###   ########.fr       */
+/*   Updated: 2023/03/11 02:46:44 by jucheval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
 int	main() {
+	{
+		Array<int> arr(15);
+		try {
+			for (uint32_t i = 0; i < 15; i++)
+				std::cout << arr[i] << std::endl;	
+		} catch (std::exception &err) {
+			std::cout << err.InvalidId() << std::endl;
+		}
+	}
 
-	Array<int> arr(15);
+	{
+		Array<int> arr = Array<int>(15);
+		for (uint32_t i = 0; i < 15; i++)
+			std::cout << arr[i] << std::endl;	
+		
+	}
+
+
 }
